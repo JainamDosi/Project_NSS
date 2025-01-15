f356393564d488680b3c31e150cc56ec3b2980a3
Jainam <127018175+JainamDosi@users.noreply.github.com>
2025-01-15 23:58:49 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';    
 
+dotenv.config();
+const PORT=process.env.PORT;
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Server responding!');
 }); 
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
   console.log('Server is running on port 5000');
 } );    
